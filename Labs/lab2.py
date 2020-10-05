dcd666b24a7d782a585f88d12d522e107fa33090
Jina Park <jinapark@Jinas-MacBook-Pro.local>
2020-10-06 00:01:01 +0100

    my lab2 solution

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -11,55 +11,72 @@ class Types_and_Strings:
     def play_with_strings(self):
         # working with strings
         message = input("Enter your noun: ")
-        print("Originally entered: "+ message)
+        print("Originally entered: ", message)
 
         #
         # Enter your own print statements below:
         #
 
         # print only first and last of the sentence:
-
+        print("\nFirst character: " + message[0])
+        print("\nLast character: " + message[-1])
 
         # use slice notation:
-
+        print("Print from position 3 to end: " + message[3:])
+        print("Print up to position 3: " + message[:3])
+        print("Prints everything via slice: " + message[:])
+        print("\nSlicing characters from 3-8: " + message[3:8])
+        print("\nSlicing characters between 5nd and 2nd last character: " + message[5:-2])
 
         # escaping a character:
-
+        print("\nHe said \"that\'s fantastic\"!")
 
         # find all a's in the input word and count how many there are:
+        lower_message = message.lower()
+        print("All lower characters: " + lower_message)
+        print("The first occurence of a is at position: " + str(lower_message.find('a')))
+        print("There are " + str(lower_message.count('a')) + " a's in the word.")
+        print("Total character count is: " + str(len(lower_message)))
 
 
         # replace all occurences of the character a with the - sign
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace():
-
+        print(lower_message.replace('a', '-'))
 
     def play_with_lists(self):
         message = input("Please enter a whole sentence: ")
         print("Originally entered: "+ message)
 
         # hand the input string to a list and print it out:
-
+        li = list(message.split(" "))
+        print(li)
 
         # append a new element to the list and print:
-
+        li.append("new")
+        print(li)
 
         # remove from the list in 3 ways:
-
+        print(li.pop())
+        print(li.remove("cake"))
+        del li[-1:-2]
+        print(li)
 
         # check if the word cake is in your input list:
-
+        print('cake' in li)
 
         # reverse the items in the list and print:
-
+        li.reverse()
+        print(li)
 
         # reverse the list with the slicing trick:
-
+        li[::-1]
+        print(li)
 
         # print the list 3 times by using multiplication:
-
+        print(li*3)
 
 
 tas = Types_and_Strings()
 tas.play_with_strings()
-#tas.play_with_lists()
+tas.play_with_lists()
