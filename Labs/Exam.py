293ab856ff4a9aaa852eaa097b0613f1048b4163
Jina Park <jinapark119@gmail.com>
2021-05-18 23:38:01 +0100

    bla

--- Labs/Exam.py
+++ Labs/Exam.py
@@ -1,9 +1,34 @@
 # iterate over a file
-fo = open('gettisburg.txt')
+fo = open('plaintext.txt', 'r')
+print(fo.read())
+
+fo = open('plaintext.txt', 'r')
 for line in fo:
     print(line)
 
+#fo = open('plaintext.txt', 'r')
+#for line in fo:
+#    line = line.splitlines()
+#print(line)
+
+
+
+
+
 # iterate over a list
-list = [1, 2, 3, 4, 5]
+list = ['cat', 'dog', 'lion', 'zerba', 'flamingo']
 for i in list:
     print(i)
+
+for index, number in enumerate(list):
+    print(index, number)
+
+# print 12 with 10 leading spaces
+str = "12"
+print(str + 10 * ' ')
+print(str.rjust(10))
+print(str.ljust(10))
+
+
+
+
---
+++ Labs/regexmatch.py
@@ -0,0 +1,43 @@
+# course: Object-oriented programming, year 2
+# academic year: 2020-21
+# author: Jina Park
+# date: 18-05-2021
+# purpose: OOP Semester 2 Exam
+
+# A function that accepts a string argument and returns a value indicating
+# if the string matches a particular regular expression.
+def find_function():
+    str = "Jina Park from South Korea"
+    result = str.find('Jina')
+    if(result != -1):
+        print("Word found in the given string")
+
+find_function()
+
+
+# Function body evaluates the argument and determines if it is a date
+# in the format DD[.|-]MM[.|-]YYYY, where the year should accept only values starting at 2000
+import datetime
+
+def validate(day, month, year):
+
+    isValid = True
+
+    try:
+        datetime.datetime(int(year), int(month), int(day))
+    
+    except ValueError:
+        isValid = False
+        #ßraise ValueError("Incorrect data format")
+
+    if(isValid):
+        print("Valid input date" )
+    if(year < 2000):
+        print("Year should be greater than 2000")
+    else:
+        print("Invalid input date")
+        
+
+validate(19, 1, 1995)
+validate(19, 15, 2022)
+
